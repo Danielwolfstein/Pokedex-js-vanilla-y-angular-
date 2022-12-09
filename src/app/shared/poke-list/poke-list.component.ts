fbb45ff4e5ea7a1d8814a253b8295ee77eafedef
Daniel Ramirez <danielramirez.ff@academia.umb.edu.co>
2022-12-09 14:29:28 -0500

    angular primera version

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/service/pokeapi.service';
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+  public getAllPokemons: any;
+  constructor(
+    private pokeApiService: PokeapiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.getAllPokemons = res.results;
+        console.log(this.getAllPokemons)
+      }
+      );
+  }
+
+}
